47d430d4aa48e06a4ae3813fa5957644e8d00af4
luka7491 <56920787+luka7491@users.noreply.github.com>
2019-10-24 13:16:29 +0200

    Add files via upload

---
+++ Math_game.py
@@ -0,0 +1,27 @@
+import random
+
+score=0
+questions = {}
+
+
+for i in range(10):
+    int_a = random.randint(0,10)
+    int_b = random.randint(0,10)
+    operators = ['+','-','*']
+    operator_value = random.choice(operators)
+    question = str(int_a)+" "+operator_value+" "+str(int_b)
+    answer = str(eval(question))
+    question+=": "
+    
+    
+    questions.update({question:answer})
+
+
+for q in questions.keys():
+    user_answer=input(q)
+    if questions.get(q)==user_answer:
+        print("Correct!")
+        score+=1
+    else:
+        print("You're Wrong!")
+print("You got "+str(score)+" right!")
