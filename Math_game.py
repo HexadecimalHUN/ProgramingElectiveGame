3c49be42b2c78c1946c57f5ca8121c7cc4f0f39f
Andrukles <54931942+Andrukles@users.noreply.github.com>
2019-10-27 14:54:38 +0100

    Update Math_game.py

--- Math_game.py
+++ Math_game.py
@@ -8,7 +8,7 @@ print ("Hello to Math game!")
 for i in range(10):
     int_a = random.randint(0,10)
     int_b = random.randint(0,10)
-    operators = ['+','-','*','/']
+    operators = ['+','-','*','//']
     operator_value = random.choice(operators)
     question = str(int_a)+" "+operator_value+" "+str(int_b)
     answer = str(eval(question))
